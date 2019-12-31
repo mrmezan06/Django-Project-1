8e52dbe621496abc148353cbe06be7fb0980a87d
mezan <mrmezan0@gmail.com>
2019-12-31 07:55:39 +0600

    Create Apps in Django

--- MyProject/ContactUs/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContactusConfig(AppConfig):
-    name = 'ContactUs'
--- MyProject/SportNews/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SportnewsConfig(AppConfig):
-    name = 'SportNews'
